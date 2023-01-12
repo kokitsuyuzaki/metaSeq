eb67b2f7fb1d39b98f50cc25620c0a957deb7167
露崎弘毅 <koki@dhcp32-179.cdb.riken.go.jp>
2023-01-12 17:23:37 +0900

    .Rbuildignore

---
+++ metaSeq/R/meta.oneside.noiseqbio.R
@@ -0,0 +1,7 @@
+meta.oneside.noiseqbio <- function(input, k = 0.5, norm = c("rpkm", "uqua", "tmm", "n"), nclust = 15, plot = FALSE, factor = NULL, conditions = NULL, lc = 0, r = 50, adj = 1.5, a0per = 0.9, random.seed = 12345, filter = 1, depth = NULL, cv.cutoff = 500, cpm = 1, studies = NULL, cl = NULL){
+
+
+
+
+
+}
--- metaSeq/inst/test_allmethods.R
+++ metaSeq/inst/test_allmethods.R
@@ -1,4 +1,3 @@
-
 ######### testdata ########
 testdata <- matrix(rpois(12*100, 500), ncol=12, nrow=100)
 test.flag1 <- c(1,1,1,0,0,0, 1,1,1,0,0,0)
@@ -11,14 +10,23 @@ cds13 <- NOISeq:::readData(data = testdata[, c(1,4)], factor = as.data.frame(tes
 cds14 <- NOISeq:::readData(data = testdata[, c(1,4:5)], factor = as.data.frame(test.flag1[c(1,4:5)]))
 
 cds2 <- meta.readData(data = testdata, factor = test.flag1, studies = test.flag2)
-expect_equivalent(length(which(as.vector(cds11@assayData$exprs) == as.vector(cds2[[1]]@assayData$exprs))), 600)
-expect_equivalent(length(which(as.vector(cds12@assayData$exprs) == as.vector(cds2[[2]]@assayData$exprs))), 600)
 
+expect_equivalent(prod(dim(cds11)), 600)
+expect_equivalent(prod(dim(cds12)), 600)
+expect_equivalent(prod(dim(cds13)), 200)
+expect_equivalent(prod(dim(cds14)), 300)
+
+expect_equivalent(prod(dim(cds2[[1]])), 600)
+expect_equivalent(prod(dim(cds2[[2]])), 600)
 
 ######### noiseq ########
+# Warningが出て、NOISeqBIOを推奨してくる
 result11 <- NOISeq:::noiseq(cds11, k = 0.5, norm = "tmm", replicates = "biological", factor = "test.flag1[1:6]", conditions = c(1, 0))
+# Warningが出て、NOISeqBIOを推奨してくる
 result12 <- NOISeq:::noiseq(cds12, k = 0.5, norm = "tmm", replicates = "biological", factor = "test.flag1[7:12]", conditions = c(1, 0))
+# Warningが出ない
 result13 <- NOISeq:::noiseq(cds13, k = 0.5, norm = "tmm", replicates = "no", factor = "test.flag1[c(1, 4)]", conditions = c(1, 0))
+# Warningが出て、NOISeqBIOを推奨してくる
 result14 <- NOISeq:::noiseq(cds14, k = 0.5, norm = "tmm", replicates = "biological", factor = "test.flag1[c(1, 4:5)]", conditions = c(1, 0))
 expect_equivalent(is(result11)[1], "Output")
 expect_equivalent(is(result12)[1], "Output")
@@ -30,11 +38,9 @@ expect_equivalent(is(result13)[2], "myInfo")
 expect_equivalent(is(result14)[2], "myInfo")
 
 ######## oneside.noiseq #########
-factors <- test.flag1
+# 環境で渡さないといけない気持ち悪い仕様
 e <- new.env()
-e$factors <- factors
-loc <- list()
-length(loc) <- 3
+e$factors <- test.flag1
 e$loc[[1]] <- 1:6
 e$loc[[2]] <- 7:12
 e$loc[[3]] <- c(1,4)
@@ -42,16 +48,20 @@ e$loc[[3]] <- c(1,4)
 result21 <- metaSeq:::oneside.noiseq(cds2[[1]], k = 0.5, norm = "tmm", replicates = "biological", factor = "e$factors[e$loc[[x]]]", conditions = c(1, 0), x = 1)
 result22 <- metaSeq:::oneside.noiseq(cds2[[2]], k = 0.5, norm = "tmm", replicates = "biological", factor = "e$factors[e$loc[[x]]]", conditions = c(1, 0), x = 2)
 
-expect_equivalent(nrow(result11@results), nrow(result21@results))
-expect_equivalent(nrow(result12@results), nrow(result22@results))
-expect_equivalent(ncol(result11@results), ncol(result21@results))
-expect_equivalent(ncol(result12@results), ncol(result22@results))
+expect_equivalent(dim(result11@results[[1]]), dim(result21@results[[1]]))
+expect_equivalent(dim(result12@results[[1]]), dim(result22@results[[1]]))
+
+######## oneside.noiseqbio #########
+# metaSeq:::oneside.noiseqbio
+result31 <- oneside.noiseqbio(cds2[[1]], k = 0.5, norm = "tmm", factor = "e$factors[e$loc[[x]]]", lc=1, r = 20, adj = 1.5, plot = FALSE, a0per = 0.9, random.seed = 12345, fileter = 2)
+
 
 ######### other.oneside.pvalues ########
 upper <- matrix(runif(300), ncol=3, nrow=100)
 lower <- matrix(runif(300), ncol=3, nrow=100)
 weight <- c(3,6,8)
 result3 <- other.oneside.pvalues(upper, lower, weight)
+
 expect_equivalent(length(result3), 9)
 expect_equivalent(length(result3[,1]), 3)
 expect_equivalent(length(result3[,2]), 3)
@@ -86,6 +96,7 @@ expect_equivalent(length(S$Lower), 100)
 expect_equivalent(length(F$Weight), 2)
 expect_equivalent(length(S$Weight), 2)
 
+# WeightなしにStouffer.testをやるとエラー
 result6 <- other.oneside.pvalues(upper, lower)
 expect_error(Stouffer.test(result6))
 
